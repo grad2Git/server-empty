cf3e2ed4c4c9564d0e7869518345be8e1ae7dd39
Erti <maaax1333@gmail.com>
2024-03-21 23:50:58 +0200

    heroku

---
+++ bin/server.dart
@@ -0,0 +1,19 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+
+void main() async {
+  var handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(_echoRequest);
+
+  var server = await serve(handler, '0.0.0.0', 8080);
+
+  server.autoCompress = true;
+}
+
+Future<Response> _echoRequest(Request request) async => Response.ok(
+  {
+    "headers": request.headers.map((key, value) => MapEntry(key, "$key:$value\n")).values.toString(),
+    "body": await request.readAsString(),
+  }.toString(),
+);
