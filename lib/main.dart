a33c47adbddd98013d2b5f051bb8eaa77634669b
Erti <maaax1333@gmail.com>
2024-03-21 18:00:26 +0200

    cors

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 void main() async {
-  var handler = const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  final overrideHeaders = {
+    ACCESS_CONTROL_ALLOW_ORIGIN: '*',
+    'Content-Type': 'application/json;charset=utf-8'
+  };
+  var handler = const Pipeline()
+      .addMiddleware(corsHeaders(headers: overrideHeaders))
+      .addMiddleware(logRequests())
+      .addHandler(_echoRequest);
 
   var server = await shelf_io.serve(handler, '0.0.0.0', 8080);
 
