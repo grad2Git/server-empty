91bd1d550641c0d2302e109134274970eeb83783
Erti <maaax1333@gmail.com>
2024-03-25 19:58:41 +0200

    port

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:shelf/shelf_io.dart';
 void main() async {
   var handler = const Pipeline()
       .addHandler(_echoRequest);
+  
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-
-  var server = await serve(handler, '0.0.0.0', int.fromEnvironment('PORT', defaultValue: 8080));
+  var server = await serve(handler, ip, port);
 
   server.autoCompress = false;
 }
